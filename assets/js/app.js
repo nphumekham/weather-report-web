12ef29a7cf92e91d60c00a8e0fa15d7fa5c55a76
nphumekham <nutchaya.phumekham@agoda.com>
2022-03-06 15:57:36 +0700

    gogo4

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,47 +1,67 @@
+//url paramerters
 var apiKey = "G9rl(3Z6evWODYwJhHWHNglpVEhRF(m3UD5yVmBUr7zgFiOCiLPbUw1Z8XG5V3DntWUW5i6X2L0mw)eKVOBOad0=====2"
-
 var latBKK = 13.7563;
 var lonBKK = 100.5018;
-
 var latCNX = 18.7677;
 var lonCNX = 98.9640;
-
 var freqDaily = "daily";
 var freq3Hours = "3Hours";
 var interval = 7;
-
 var lat = latBKK;
 var lon = lonBKK;
 var frequency = freqDaily;
-
-var locationName = "";
+//charts info
 var tempForYAxis = [];
 var iconForXAxis = [];
 var countBars = 7;
-const buttons = document.querySelectorAll('.button');
-buttons.forEach((button) => {
-  let target = button.querySelector('.target');
-  function handleMove(e) {
-    const x = -50 + (e.pageX - button.offsetLeft - 300 / 2) / 3;
-    const y = -10 + (e.pageY - button.offsetTop - 100 / 2) / 3;
-
-    target.style.setProperty('--x', `${ x }px`)
-    target.style.setProperty('--y', `${ y }px`)
-  }
-  button.addEventListener('mousemove', (e) => {
-    handleMove(e);
-  });
-  button.addEventListener('touchmove', (e) => {
-    handleMove(e.changedTouches[0]);
-  });
+//header info
+var headerLocation = document.getElementById("location");
+var headerFrequency = document.getElementById("frequency");
+//button colors 
+var buttons = document.querySelectorAll("button");
+function changeButtons(index) {
+	if (countBars == 7) {
+		buttons[2].classList.remove('notclicked');
+		buttons[2].classList.add('clicked');
+		buttons[3].classList.remove('clicked');
+		buttons[3].classList.add('notclicked');
+	}
+	if (countBars == 8) {
+		buttons[3].classList.remove('notclicked');
+		buttons[3].classList.add('clicked');
+		buttons[2].classList.remove('clicked');
+		buttons[2].classList.add('notclicked');
+	}
+	if (lat == latBKK) {
+		buttons[0].classList.remove('notclicked');
+		buttons[0].classList.add('clicked');
+		buttons[1].classList.remove('clicked');
+		buttons[1].classList.add('notclicked');
+	}
+	if (lat == latCNX) {
+		console.log("in color cnx");
+		console.log(buttons[1].classList);
+		buttons[1].classList.remove('notclicked');
+		buttons[1].classList.add('clicked');
+		buttons[0].classList.remove('clicked');
+		buttons[0].classList.add('notclicked');
+	}
+}
+//load while wait for ajax
+$body = $("body");
+$(document).on({
+    ajaxStart: function() { $body.addClass("loading");    },
+     ajaxStop: function() { $body.removeClass("loading"); }    
 });
+//change paramerters each push
 function bkkChart() {
 	console.log("bkk chart");
 	tempForYAxis = [];
 	iconForXAxis = [];
 	lat = latBKK;
 	lon = lonBKK;
-	buildJSON(countBars)
+	changeButtons(0);
+	buildJSON(countBars);
 }
 function cnxChart() {
 	console.log("cnx chart");
@@ -49,7 +69,8 @@ function cnxChart() {
 	iconForXAxis = [];
 	lat = latCNX;
 	lon = lonCNX;
-	buildJSON(countBars)
+	changeButtons(1);
+	buildJSON(countBars);
 }
 function dailyChart() {
 	console.log("daily chart");
@@ -57,7 +78,9 @@ function dailyChart() {
 	iconForXAxis = [];
 	frequency = freqDaily;
 	countBars = 7;
-	buildJSON(countBars)
+	changeButtons(2);
+
+	buildJSON(countBars);
 }
 function threeHoursChart() {
 	console.log("three hours chart");
@@ -65,9 +88,10 @@ function threeHoursChart() {
 	iconForXAxis = [];
 	frequency = freq3Hours;
 	countBars = 8;
-	buildJSON(countBars)
+	changeButtons(3);
+	buildJSON(countBars);
 }
-var tags = [];
+//build JSON for chart
 function buildJSON(countBars) {
 	console.log("in render chart");
 	$.ajax({
@@ -86,16 +110,25 @@ function buildJSON(countBars) {
 			console.log("this is success");
 			console.log(results);
 			var weatherResults = results.results.weather;
-			locationName = results.results.locationName;
-			for(var i=0; i<countBars; i++){
+			var locationName = results.results.locationName;
+			for (var i = 0; i < countBars; i++) {
 				tempForYAxis.push(weatherResults[i].temperature.temp);
 				iconForXAxis.push(weatherResults[i].icon);
 			}
-			console.log("prepare axis array");
-			console.log(tempForYAxis);
-			console.log(iconForXAxis);
-
-			renderChart(iconForXAxis, tempForYAxis);
+			headerLocation.innerHTML = "<h3>" + locationName + "</h3>";
+			headerFrequency.innerHTML = "<h6>Frequency: " + frequency + "</h6>";
+			var barColors = [];
+			var green = "#06A77D";
+			var yellow = "#F1A208";
+			var red = "#F93943";
+			var blue = "#7EB2DD";
+			for (var i = 0; i < countBars; i++) {
+				if (tempForYAxis[i] < 30) { barColors.push(blue); }
+				else if (tempForYAxis[i] > 30 && tempForYAxis[i] < 35) { barColors.push(green); }
+				else if (tempForYAxis[i] > 35 && tempForYAxis[i] < 40) { barColors.push(yellow); }
+				else { barColors.push(red); }
+			}
+			renderChart(iconForXAxis, tempForYAxis, barColors);
 		},
 		error: function (response) {
 			console.log("this is error");
@@ -103,123 +136,59 @@ function buildJSON(countBars) {
 		}
 	});
 }
-function renderChart(xAxisme, yAxisme) {
-	console.log("in render chart");
-	console.log(countBars);
-	console.log(xAxisme);
-	var barColors = [];
-	if(countBars==7){
-		barColors = ["red", "green", "blue", "orange", "brown", "orange", "brown"];
-	}
-	else{barColors = ["red", "green", "blue", "orange", "brown", "orange", "brown", "brown"];}
-	var xValues = ["Italy", "France", "Spain", "USA", "Argentina", "USA", "Argentina"];
-	
+//render chart
+function renderChart(icon, temperature, barColors) {
 	new Chart(document.getElementById("myChart"), {
 		type: "bar",
 		plugins: [{
-		  afterDraw: chart => {      
-			var ctx = chart.chart.ctx; 
-			var xAxis = chart.scales['x-axis-0'];
-			var yAxis = chart.scales['y-axis-0'];
-			xAxis.ticks.forEach((value, index) => {  
-			  var x = xAxis.getPixelForTick(index);      
-			  var image = new Image();
-			  image.src = xAxisme[index],
-			  ctx.drawImage(image, x + 30, yAxis.bottom + 5);
-			});      
-		  }
+			afterDraw: chart => {
+				var ctx = chart.chart.ctx;
+				var xAxis = chart.scales['x-axis-0'];
+				var yAxis = chart.scales['y-axis-0'];
+				xAxis.ticks.forEach((value, index) => {
+					var x = xAxis.getPixelForTick(index);
+					var image = new Image();
+					image.src = icon[index],
+						ctx.drawImage(image, x + 30, yAxis.bottom + 5);
+				});
+			}
 		}],
 		data: {
-		  labels: xAxisme,
-		  datasets: [{
-			label: locationName,
-			data: yAxisme,
-			backgroundColor: barColors
-		  }]
+			labels: temperature,
+			datasets: [{
+				label: "Temperature:",
+				data: temperature,
+				backgroundColor: barColors
+			}]
 		},
 		options: {
-		  responsive: true,
-		  layout: {
-			padding: {
-			  bottom: 50
-			}
-		  },
-		  legend: {
-			display: false
-		  },    
-		  scales: {
-			yAxes: [{ 
-			  ticks: {
-				beginAtZero: true
-			  }
-			}],
-			xAxes: [{
-			  ticks: {
+			responsive: true,
+			layout: {
+				padding: {
+					bottom: 50
+				}
+			},
+			legend: {
 				display: false
-			  }   
-			}],
-		  }
+			},
+			scales: {
+				yAxes: [{
+					ticks: {
+						beginAtZero: true
+					}
+				}],
+				xAxes: [{
+					ticks: {
+						display: false
+					}
+				}],
+			}
 		}
-	  });
+	});
 }
+//main
 var main = function () {
 	"use strict";
 	buildJSON(countBars);
-	console.log("this is tags");
-	console.log(tags);
-	var xValues = ["Italy", "France", "Spain", "USA", "Argentina"];
-	var yValues = [55, 49, 44, 24, 900];
-
-	
-	var toDos = [
-		"Finish writing this book",
-		"Take Gracie to the park",
-		"Answer emails",
-		"Prep for Monday's class",
-		"Make up some new ToDos",
-		"Get Groceries"
-	];
-
-	$(".tabs span").toArray().forEach(function (element) {
-		$(element).on("click", function () {
-			var $element = $(element), $content;
-			$(".tabs span").removeClass("active");
-			$element.addClass("active");
-			$(".content").empty();
-
-			if ($element.parent().is(":nth-child(1)")) {
-				console.log("First tab clicked.");
-				$content = $("<ul>");
-				var i;
-				for (i = toDos.length - 1; i >= 0; i--) {
-					$content.append($("<li>").text(toDos[i]));
-				}
-			}
-			else if ($element.parent().is(":nth-child(2)")) {
-				$content = $("<ul>");
-				toDos.forEach(function (todo) {
-					$content.append($("<li>").text(todo));
-				});
-			}
-			else if ($element.parent().is(":nth-child(3)")) {
-				// input a new to-do
-				var $input = $("<input>"),
-					$button = $("<button>").text("+");
-
-				$button.on("click", function () {
-					if ($input.val() !== "") {
-						toDos.push($input.val());
-						$input.val("");
-					}
-				});
-
-				$content = $("<div>").append($input).append($button);
-
-			}
-			$(".content").append($content);
-			return false;
-		});
-	});
-	$(".tabs a:first-child span").trigger("click");
 }
 $(document).ready(main);
